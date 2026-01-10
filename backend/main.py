7b2a51dc87e6ad0ce55770a77f613be1f3746df1
aubreyasta <samuderabagas1@gmail.com>
2026-01-10 14:01:18 -0800

    feat: Implemented preprocessing for optimized search performance. Load and index all songs at startup. Added CORS middleware setup to link API with frontend in preparation.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import json
 from fuzzywuzzy import fuzz
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
