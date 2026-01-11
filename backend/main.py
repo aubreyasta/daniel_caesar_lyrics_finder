6606d63f25599d11b9be604829a5d965ed2c2255
aubreyasta <samuderabagas1@gmail.com>
2026-01-10 23:04:05 -0800

    Deployment prep

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://daniel-caesar-lyrics-search.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
